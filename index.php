b9b819239f9f83c0254c221929e5210357c71496
Vivien Leroy <vivienleroy@me.com>
2012-06-26 23:27:39 +0200

    Ignore *.swp (vim), virage de end.php et dégageage des tags pour Analytics faire du A/B

--- index.php
+++ index.php
@@ -8,48 +8,6 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	<?php
-		if($_GET['variation'] != 1){?>
-	<!-- Google Website Optimizer Control Script -->
-	<script>
-	function utmx_section(){}function utmx(){}
-	(function(){var k='2049003810',d=document,l=d.location,c=d.cookie;function f(n){
-	if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return escape(c.substring(i+n.
-	length+1,j<0?c.length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;
-	d.write('<sc'+'ript src="'+
-	'http'+(l.protocol=='https:'?'s://ssl':'://www')+'.google-analytics.com'
-	+'/siteopt.js?v=1&utmxkey='+k+'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='
-	+new Date().valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
-	'" type="text/javascript" charset="utf-8"></sc'+'ript>')})();
-	</script><script>utmx("url",'A/B');</script>
-	<!-- End of Google Website Optimizer Control Script -->
-	<!-- Google Website Optimizer Tracking Script -->
-	<script type="text/javascript">
-	  var _gaq = _gaq || [];
-	  _gaq.push(['gwo._setAccount', 'UA-25140503-3']);
-	  _gaq.push(['gwo._trackPageview', '/2049003810/test']);
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	</script>
-	<!-- End of Google Website Optimizer Tracking Script -->
-	<?php } else { ?>
-	<!-- Google Website Optimizer Tracking Script -->
-	<script type="text/javascript">
-	  var _gaq = _gaq || [];
-	  _gaq.push(['gwo._setAccount', 'UA-25140503-3']);
-	  _gaq.push(['gwo._trackPageview', '/2049003810/test']);
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	</script>
-	<!-- End of Google Website Optimizer Tracking Script -->
-	
-	<?php } ?>
 		<meta charset="UTF-8" />
 		<meta name="google-site-verification" content="XXCumqKxxWp7uXo_BSe7ZMRC8wdPFnI0OPKQ65JNiQw" />
 		<link rel="icon" type="image/png" href="ressources/images/favicon.png" />
