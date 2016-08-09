7b2857dee9f6011c8056181dea9f5378fccc9c76
Vivien Leroy <vivienleroy@me.com>
2016-08-09 03:11:15 +0200

    HAML FTW

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source "https://rubygems.org"
 
 gem 'jekyll'
-gem 'jekyll-multiple-languages-plugin'
+gem 'redcarpet'
+
+group :jekyll_plugins do
+	gem 'jekyll-haml'
+end
+
--- script/script.js
+++ script/script.js
@@ -1,34 +1,26 @@
 var pfx = ["webkit", "moz", "MS", "o", ""];
 
-function PrefixedEvent(element, type, callback)
-{
-    for (var p = 0; p < pfx.length; p++)
-    {
+function PrefixedEvent(element, type, callback) {
+    for (var p = 0; p < pfx.length; p++) {
         if (!pfx[p]) type = type.toLowerCase();
         element.addEventListener(pfx[p]+type, callback, false);
     }
 }
 
 var a = document.createElement('audio');
-if(!!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, '')))
-{
+if (!!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''))) {
 	var snd = new Audio('/resources/gb.mp3');
-}
-else
-{
-	var snd = new Audio('/resources/gb.ogg');
+} else {
+    var snd = new Audio('/resources/gb.ogg');
 }
 
-snd.addEventListener('ended', function(event)
-{
+snd.addEventListener('ended', function(event) {
     console.log('Audio finished playing, yes man this was the Game Boy startup chime :P.');
 }, false);
 
-document.addEventListener("DOMContentLoaded", function(){
-
-    if(document.getElementById('header_text'))
-    {
-        PrefixedEvent(document.getElementById('header_text'), 'TransitionEnd', function(event){
+document.addEventListener("DOMContentLoaded", function() {
+    if (document.getElementById('header_text')) {
+        PrefixedEvent(document.getElementById('header_text'), 'TransitionEnd', function(event) {
             snd.play();
         });
         
